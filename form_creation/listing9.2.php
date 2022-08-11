6cc6291838d7d665fad2a3b101b519c9e9ec0a10
Dimitris Marg <97585467+dimitrisftth1@users.noreply.github.com>
2022-08-11 12:19:01 +0300

    form creation

---
+++ form_creation/listing9.2.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Listing 9.2 Reading input from a form </title>
+</head>
+<body>
+<?php
+echo "<p>Welcome <b>$_POST[user]</b></p>";
+echo "<p>Your address is:<br><b>$_POST[address]</b></p>";
+?>
+</body>
+</html>
