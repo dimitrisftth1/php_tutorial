6217370d2a47cd35c9d2095c2138fa1284d9c8f0
Dimitris Marg <97585467+dimitrisftth1@users.noreply.github.com>
2026-05-19 17:40:07 +0300

    Add files via upload

---
+++ variable1.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Variable1 Lesson100326</title>
+</head>
+<body>
+<?php
+$a=10;
+$a_long_is_variable_name;
+$variable2453;
+$sleepyZZZZ;
+
+echo $a;
+echo $a_long_is_variable_name;
+
+?>
+</body>
+</html>
